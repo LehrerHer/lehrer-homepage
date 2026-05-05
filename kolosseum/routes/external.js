2594c0af1f860d82de98827f9b48a88c5ba09269
Jan Herrmann <jan@lehrer-herrmann.de>
2026-05-05 15:46:44 +0000

    Deutsch-Projekte: Unterseiten für Das Parfum und Theaterprojekt Jahrgang 9 anlegen

--- kolosseum/routes/external.js
+++ kolosseum/routes/external.js
@@ -5,12 +5,14 @@ const { checkAndAwardBadges } = require('../db/badges');
 
 const router = express.Router();
 
-const VALID_SLUGS = ['stilmittel', 'literaturwissenschaft', 'rechtschreibung', 'lernquiz-jahrgang5'];
+const VALID_SLUGS = ['stilmittel', 'literaturwissenschaft', 'rechtschreibung', 'lernquiz-jahrgang5', 'das-parfum', 'theaterprojekt-9'];
 const QUIZ_LABELS = {
   stilmittel:            'Stilmittel-Quiz',
   literaturwissenschaft: 'Literaturwissenschaft-Quiz',
   rechtschreibung:       'Rechtschreib-Quiz',
   'lernquiz-jahrgang5':  'Lernquiz Jahrgang 5',
+  'das-parfum':          'Das Parfum',
+  'theaterprojekt-9':    'Theaterprojekt Jahrgang 9',
 };
 
 // Notenpunkte-Tabelle (Oberstufe, 0–15 Punkte)
